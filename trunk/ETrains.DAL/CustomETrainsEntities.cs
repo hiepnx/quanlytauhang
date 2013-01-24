23f1fe30b72dee316a45c5550405a738397d8e61
cuongtrinhdinh87@gmail.com <cuongtrinhdinh87@gmail.com@d37d60cb-82b0-833c-f39b-763f2fcdc4fa>
2013-01-24 01:00:21 +0000

    update Train

--- trunk/ETrains.DAL/CustomETrainsEntities.cs
+++ trunk/ETrains.DAL/CustomETrainsEntities.cs
@@ -118,4 +118,82 @@ namespace ETrains.DAL
             }
         }
     }
+
+    public partial class tblToKhaiTau : EntityObject
+    {
+        private string _createdByName;
+        private string _modifiedByName;
+        private string _typeCodeName;
+        private string _companyName;
+
+        public string Ma_Chuyen_Tau
+        {
+            get
+            {
+                return this.tblChuyenTau.Ma_Chuyen_Tau;
+            }
+        }
+        public string CreatedByName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_createdByName))
+                {
+                    var db = DataAccess.Instance();
+                    _createdByName = db.tblUsers.Where(x => x.UserID == CreatedBy).FirstOrDefault().Name;
+                }
+                return _createdByName;
+            }
+        }
+        public string ModifiedByName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_modifiedByName))
+                {
+                    if (ModifiedBy == null) _modifiedByName = string.Empty;
+                    else
+                    {
+                        var db = DataAccess.Instance();
+                        _modifiedByName = db.tblUsers.Where(x => x.UserID == ModifiedBy).FirstOrDefault().Name;
+                    }
+                }
+                return _modifiedByName;
+            }
+        }
+
+        public string TypeCodeName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_typeCodeName))
+                {
+                    if (string.IsNullOrEmpty(TypeCode)) _typeCodeName = string.Empty;
+                    else
+                    {
+                        var db = DataAccess.Instance();
+                        _typeCodeName = db.tblTypes.Where(x => x.TypeCode == TypeCode).FirstOrDefault().TypeName;
+                    }
+                }
+                return _typeCodeName;
+            }
+        }
+
+        public string CompanyName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_companyName))
+                {
+                    if (string.IsNullOrEmpty(CompanyName)) _companyName = string.Empty;
+                    else
+                    {
+                        var db = DataAccess.Instance();
+                        _companyName = db.tblCompanies.Where(x => x.CompanyCode == CompanyCode).FirstOrDefault().CompanyName;
+                    }
+                }
+                return _companyName;
+            }
+        }
+    }
 }
