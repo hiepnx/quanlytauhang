1e8aa3e580ce10448146a731a3d28caf136a1f76
vuongfat@gmail.com <vuongfat@gmail.com@d37d60cb-82b0-833c-f39b-763f2fcdc4fa>
2013-03-31 16:14:51 +0000

    update logo + icon

--- ETrains/FrmPreviewReport.Designer.cs
+++ ETrains/FrmPreviewReport.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPreviewReport));
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.SuspendLayout();
             // 
@@ -48,6 +49,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 328);
             this.Controls.Add(this.crystalReportViewer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmPreviewReport";
             this.Text = "FrmPreviewReport";
             this.ResumeLayout(false);
