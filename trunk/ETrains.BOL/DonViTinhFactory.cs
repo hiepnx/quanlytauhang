c39cca5693457cd8b656358546b5e9e7a296b2a8
tungdt.dtt@gmail.com <tungdt.dtt@gmail.com@d37d60cb-82b0-833c-f39b-763f2fcdc4fa>
2013-05-30 15:32:29 +0000

    Fixed: 2. Khi cap nhat danh sach toa tau e de truong "don vi tinh" la danh sach lua chon theo danh muc, chu k nhap bang tay thu cong vao. ly do: de thong nhat danh muc don vi tinh khi in BBBG.

---
+++ trunk/ETrains.BOL/DonViTinhFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ETrains.DAL;
+using ETrains.Utilities;
+using System.Configuration;
+using System.Data;
+using log4net;
+
+namespace ETrains.BOL
+{
+    public class DonViTinhFactory
+    {
+        public static List<tblDonViTinh> getAll()
+        {
+            var db = new dbTrainEntities(ConnectionController.GetConnection());
+
+            return db.tblDonViTinhs.ToList();
+        }
+    }
+}
